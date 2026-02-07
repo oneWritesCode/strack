6d0d87e5fde06a601d3880691c533d3cf7993234
onewritescode <lalmohansingh83@gmail.com>
2026-02-07 14:43:16 +0530

    fixed login issue and design

--- app/components/LoginPage.tsx
+++ app/components/LoginPage.tsx
@@ -6,33 +6,13 @@ import InstallPWA from "./InstallPWA";
 function LoginPage() {
   return (
     <div className="w-full min-h-screen bg-[#8B5CF6] flex items-center justify-center flex-col text-white overflow-hidden">
-     
-      <div className="px-10 py-4 flex flex-col w-full items-center gap-5 justify-center font-bubblegum font-light">
-        <div className="relative flex borde">
-          <img
-            src="/images/alternateUserImageBlack.png"
-            alt=""
-            className="rounded-full rotate-40 translate-x-15 sm:translate-x-25 z-10"
-          />{" "}
-          <img
-            src="/images/alternateUserImageGirl.png"
-            alt=""
-            className="absolute rounded-full -rotate-40 scale-110 sm:scale-100  -translate-x-13 -translate-y-25 sm:-translate-x-25 sm:-translate-y-35 z-10"
-          />{" "} 
-          <img
-            src="/images/alternateUserImageL.png"
-            alt=""
-            className="absolute scale:70 sm:scale-50 -rotate- translate-x-20 -translate-y-32 sm:translate-x-25 sm:-translate-y-44 z-10"
-          />
-          <img
-            src="/images/luffy.png"
-            alt=""
-            className="absolute rounded-full sm:scale-100 rotate-30 -translate-x-18 translate-y-8 sm:-translate-x-30 sm:translate-y-20 z-10"
-          />
+      <div className="flex flex-col w-full items-center gap-3 md:gap-4 justify-center font-bubblegum font-light">
+        <div>
+          <img src="/login-page-image.png" alt="" className="md:scale-115" />
         </div>
 
         <button
-          className="pl-2 pr-10 py-2 rounded-xl border-3 border-black bg-white cursor-pointer group hover:shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)] shadow-[6px_6px_2px_3px_rgba(0,0,0,0.3)] capitalize flex items-center gap-4 font-bold transition-all duration-100"
+          className="pl-2 pr-10 py-2 rounded-xl border-3 border-black bg-white cursor-pointer group hover:shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)] shadow-[6px_6px_2px_3px_rgba(0,0,0,0.3)] capitalize flex items-center gap-4 font-bold transition-all duration-100 z-1000"
           onClick={() => signIn("google")}
         >
           <img src="/images/GoogleLogo.png" alt="logo" className="w-6 h-6" />
@@ -41,8 +21,8 @@ function LoginPage() {
           </span>
         </button>
 
-        <div className="cursor-pointer ">
-          <InstallPWA forLogin={true}/>
+        <div className="cursor-pointer z-1000">
+          <InstallPWA forLogin={true} />
         </div>
       </div>
     </div>
